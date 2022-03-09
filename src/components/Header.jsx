f700c9c05b56175ed5e9676ac1a84be880594aad
vov9ndra <vovan.larin72@gmail.com>
2022-03-09 22:05:08 +0500

    12

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://japanauto.ru/_upload/dc3c/Toyota-logo-1989-2560x1440[1].png'></img>
+    </header>
+
+}
+
+export default Header
